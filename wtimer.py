9c2d2a2e6fae57bae1b7bacbbde1ea5c8909372b
Chris Taylor <25781272+atroly@users.noreply.github.com>
2018-07-10 13:20:02 +0100

    Tidy code

--- wtimer.py
+++ wtimer.py
@@ -8,6 +8,7 @@ mp3file = '/home/pi/Sounds/klaxon.mp3'
 mp3start = '/home/pi/Sounds/started.mp3'
 mp3stop = '/home/pi/Sounds/stopped.mp3'
 gpio_pin = 21
+set_time = 18 # 1800 seconds = 30 minutes
 
 import tkinter as tk
 import os
@@ -19,6 +20,7 @@ audio = MP3(mp3file)
 
 playing = False
 running = False
+t = set_time
 
 root = tk.Tk()
 root.attributes("-fullscreen", True)
@@ -28,49 +30,42 @@ root.bind("<Escape>", lambda event:root.destroy()) # keyboard escape
 clock = tk.Label(root, font=('piboto', 256, 'bold'))
 clock.grid(row = 10, column = 2, padx = 5, pady = 5)
 
-def play():
-    subprocess.Popen(['mpg123', mp3file])            
+def play(): # begin playing alert sound and return finish time
+    subprocess.Popen(['mpg123', '-q', mp3file])            
     return time.time() + audio.info.length
 
-def reset_timer():
-    global t
-    t = 1800 # 1800 seconds = 30 minutes
-
-def button_push(chan):
-    global playing, running
+def button_push(chan): # detect button push and take appropriate action
+    global t, playing, running
     if playing:
         subprocess.call(['killall', 'mpg123'])
         playing = False
         running = False
     elif running:
         running = False
-        subprocess.Popen(['mpg123', mp3stop])
+        subprocess.Popen(['mpg123', '-q', mp3stop])
     else:   
         running = True
-        subprocess.Popen(['mpg123', mp3start])
-    reset_timer()    
+        subprocess.Popen(['mpg123', '-q', mp3start])
+    t = set_time    
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback=button_push, bouncetime=500)
-      
-reset_timer()
+GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback=button_push, bouncetime=300)
 
-def count():
+def count(): # count down and play alert when timer reaches zero
     global t, playing, running, endTime
     mins, secs = divmod(t, 60)
     timeformat = '{:02d}:{:02d}'.format(mins, secs)
-    clock.config(text=timeformat)
+    clock.config(text=timeformat) # update time display
     if running:
         t -= 1
-    if (t == 0):
+    if (t == 0): # timer has run out so play alert
         playing = True
         running = False
         endTime = play()
-    if (playing and time.time() > endTime):
+    if (playing and time.time() > endTime): # alert finished so restart
         endTime = play()
-    clock.after(1000, count)
+    clock.after(1000, count) # recurse after 1 second
  
 count()
-
 root.mainloop()
